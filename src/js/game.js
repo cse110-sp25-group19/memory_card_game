db742378e90a3d8ebd6783971148dc62cbc07942
Mohammed Azab <mazab2003@gmail.com>
2025-05-14 05:36:58 -0700

    feat(core): add utils.js & game.js stubs with JSDoc
    fixes #19

---
+++ src/js/game.js
@@ -0,0 +1,21 @@
+import { shuffleDeck } from './utils.js';
+
+/**
+ * Initializes a new game round.
+ *
+ * @returns {void}
+ */
+export function initGame() {
+  // TODO: build deck, call shuffleDeck, etc.
+}
+
+/**
+ * Renders the card grid into the supplied container.
+ *
+ * @param   {HTMLElement} container DOM node that will hold the cards
+ * @param   {Array}       deck      Card array created by initGame()
+ * @returns {void}
+ */
+export function renderBoard(container, deck) {
+  // TODO: inject placeholder elements
+}
